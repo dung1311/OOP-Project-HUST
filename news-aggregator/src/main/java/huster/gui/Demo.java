f8cd20aae379e8e657b8ed6b98185ad2cefbe4d3
Yangtai2504 <ntduong.hatinh@gmail.com>
2024-03-29 20:15:35 +0700

    123123
    123123123123

---
+++ news-aggregator/src/main/java/huster/gui/Demo.java
@@ -0,0 +1,21 @@
+package huster.gui;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("123");
+
+        System.out.println("123");
+
+        System.out.println("123");
+
+        System.out.println("123");
+
+        System.out.println("123");
+
+        System.out.println("123");
+
+        System.out.println("123");
+        System.out.println("123");
+
+    }
+}
