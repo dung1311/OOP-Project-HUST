4164e9469e75ad4433e10ebae7b1d542a7997729
Yangtai2504 <ntduong.hatinh@gmail.com>
2024-03-31 21:50:42 +0700

    1231231
    23123123

--- news-aggregator/src/main/java/huster/Main.java
+++ news-aggregator/src/main/java/huster/Main.java
@@ -1,7 +1,10 @@
 package huster;
 
+import huster.gui.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Menu menu = new Menu();
+       menu.setVisible(true);
     }
 }
