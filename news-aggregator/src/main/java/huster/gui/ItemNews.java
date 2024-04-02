30a33a229386f38579ce348e6d55b5d084754a12
Chilee <HIEU.HM224981@sis.hust.edu.vn>
2024-04-02 22:02:57 +0700

    coindesk
    1

---
+++ news-aggregator/src/main/java/huster/gui/ItemNews.java
@@ -0,0 +1,68 @@
+package com.example;
+
+public class ItemNews {
+    private String link;
+    private String source;
+    private String title;
+    private String category;
+    private String summary;
+    private String author;
+    private String date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+}
