87fc5678853dfaf265c0478af9f85d262737ac9d
Chilee <HIEU.HM224981@sis.hust.edu.vn>
2024-04-23 13:20:41 +0700

    tét da va

--- news-aggregator/src/main/java/huster/crawl/CrawlTwitter.py
+++ news-aggregator/src/main/java/huster/crawl/CrawlTwitter.py
@@ -1,25 +1,22 @@
-from ntscraper import Nitter 
-import pandas as pd 
+from ntscraper import Nitter
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
-import os
 
 scraper = Nitter(log_level=1, skip_instance_check=False)
 
 def crawl_tweet(name, mode, amount, file_name):
-    tweetList_full = scraper.get_tweets(
+    list_tweet = scraper.get_tweets(
         name, mode=mode, number=amount, max_retries=100)
-    
-    if not os.path.exists(f'news-aggregator\\recourse\\data\\{file_name}.json'):
-        with open(file=f'news-aggregator\\recourse\\data\\{file_name}.json', mode='w') as file_json:
-            json.dump(tweetList_full['tweets'], file_json)
-    else:
-        with open(file=f'news-aggregator\\recourse\\data\\{file_name}.json', mode='w') as file_json:
-            json.dump(tweetList_full['tweets'], file_json)
+
+    with open(file=f'news-aggregator\\recourse\\data\\{file_name}.json', mode='w') as file_json:
+        json.dump(list_tweet['tweets'], file_json)
+
 
 def draw_table(file_name):
-    list_tweet = pd.read_json(path_or_buf=f'news-aggregator\\recourse\\data\\{file_name}.json')
+    list_tweet = pd.read_json(
+        path_or_buf=f'news-aggregator\\recourse\\data\\{file_name}.json')
     data_list = []
     for index, tweet in list_tweet.iterrows():
         try:
@@ -36,10 +33,10 @@ def draw_table(file_name):
 
     data_list_pd = pd.DataFrame(
         data_list, columns=['time', 'comment', 'retweet', 'quote', 'like'])
-    
+
     data_list_pd = data_list_pd.sort_values(by='time')
-    
-    fig, ax = plt.subplots(figsize=(20, 10))
+
+    _, ax = plt.subplots(figsize=(20, 10))
     ax.plot(data_list_pd['time'], data_list_pd['comment'],
             label='comment', marker='o')
     ax.plot(data_list_pd['time'], data_list_pd['retweet'],
@@ -56,8 +53,8 @@ def draw_table(file_name):
     plt.legend()
     plt.show()
 
-#test
-#draw_table('news-aggregator\\recourse\\data\\tweets.json')
-# file_name = 'abc'
-# crawl_tweet('Bitcoin', mode='user', amount=10, file_name=file_name)
-# draw_table(file_name)
+
+# test
+file_name = 'abcd'
+crawl_tweet('Bitcoin', mode='user', amount=10, file_name=file_name)
+draw_table(file_name)
